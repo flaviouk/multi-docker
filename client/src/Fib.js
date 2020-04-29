79fe6c684fc53a5c40d0fbc708b28984091abb85
Flávio Carvalho <flavio@imflavio.com>
2020-04-29 18:30:16 +0100

    Minor fixes

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { isArray, isObject } from 'lodash'
 
 class Fib extends Component {
   state = {
@@ -15,14 +16,18 @@ class Fib extends Component {
 
   async fetchValues() {
     const values = await axios.get('/api/values/current');
-    this.setState({ values: values.data });
+    if (isObject(values.data)) {
+      this.setState({ values: values.data });
+    }
   }
 
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
-    this.setState({
-      seenIndexes: seenIndexes.data
-    });
+    if (isArray(seenIndexes.data)) {
+      this.setState({
+        seenIndexes: seenIndexes.data
+      });
+    }
   }
 
   handleSubmit = async event => {
@@ -35,7 +40,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes ? this.state.seenIndexes.map(({ number }) => number).join(', ') : '';
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
